28fe1273a480516944864128546e53fadd15ad05
Lucas Nunes <lucasnunes000@hotmail.com>
2019-04-06 11:41:02 -0300

    Adicionado Classe de boot da Aplicação

---
+++ src/main/java/com/poll/api/PollApiApplication.java
@@ -0,0 +1,15 @@
+package com.poll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+public class PollApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollApiApplication.class, args);
+	}
+
+}
